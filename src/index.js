a6eb107cba370945735dc2b8b16e2fbe6d306922
Matthew Conlen <mc@mathisonian.com>
2016-01-27 18:44:45 -0500

    add  flag

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ var preprocess = require('./preprocess');
 module.exports = function (_d3) {
 
   var current = browser;
+
+  var isPreprocessing = false;
   // Electron
   if (navigator.userAgent && navigator.userAgent.indexOf('Electron') > -1){
     current = preprocess;
+    isPreprocessing = true;
   }
 
   current.setD3(_d3);
 
   return {
     start: current.start,
-    stop: current.stop
+    stop: current.stop,
+    isPreprocessing: isPreprocessing
   };
 };
