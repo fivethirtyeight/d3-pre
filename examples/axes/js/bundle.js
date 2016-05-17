9c0ae4d774f08e4c1caf9f3bff4c518336cb76d6
Matthew Conlen <mc@mathisonian.com>
2016-05-17 17:23:37 -0400

    axis

--- examples/axes/js/bundle.js
+++ examples/axes/js/bundle.js
@@ -24170,9 +24170,10 @@ hljs.configure({
 
 hljs.initHighlightingOnLoad();
 
-require('./stream');
+// require('./stream');
+require('./axis');
 
-},{"./stream":164,"highlight.js":7}],164:[function(require,module,exports){
+},{"./axis":164,"highlight.js":7}],164:[function(require,module,exports){
 'use strict';
 
 var d3 = require('d3');
@@ -24186,73 +24187,60 @@ var prerender = Prerender(d3);
 // aware of SVGs that already exist on the page.
 prerender.start();
 
-var n = 20, // number of layers
-    m = 200, // number of samples per layer
-    stack = d3.layout.stack().offset("wiggle"),
-    layers0 = stack(d3.range(n).map(function() { return bumpLayer(m); })),
-    layers1 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
 
-var width = 960,
-    height = 500;
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
-    .domain([0, m - 1])
+    .domain([-width / 2, width / 2])
     .range([0, width]);
 
 var y = d3.scale.linear()
-    .domain([0, d3.max(layers0.concat(layers1), function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
+    .domain([-height / 2, height / 2])
     .range([height, 0]);
 
-var color = d3.scale.linear()
-    .range(["#aad", "#556"]);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickSize(-height);
 
-var area = d3.svg.area()
-    .x(function(d) { return x(d.x); })
-    .y0(function(d) { return y(d.y0); })
-    .y1(function(d) { return y(d.y0 + d.y); });
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(5)
+    .tickSize(-width);
 
-d3.select('#interactive').append('button').text('Update').on('click', transition);
+var zoom = d3.behavior.zoom()
+    .x(x)
+    .y(y)
+    .scaleExtent([1, 32])
+    .on("zoom", zoomed);
 
 var svg = d3.select("#interactive").append("svg")
-    .attr('viewBox', '0 0 ' + width + ' ' + height);
+    .attr("viewBox", ' 0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-if (prerender.isPreprocessing) {
-  svg.selectAll("path")
-      .data(layers0)
-    .enter().append("path")
-      .attr("d", area)
-      .style("fill", function() { return color(Math.random()); });
-}
+svg.append("rect")
+    .attr("width", width)
+    .attr("height", height);
 
-function transition() {
-  d3.selectAll("path")
-      .data(function() {
-        var d = layers1;
-        layers1 = layers0;
-        return layers0 = d;
-      })
-    .transition()
-      .duration(2500)
-      .attr("d", area);
-}
+var xg = svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-// Inspired by Lee Byron's test data generator.
-function bumpLayer(n) {
+var yg = svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
 
-  function bump(a) {
-    var x = 1 / (.1 + Math.random()),
-        y = 2 * Math.random() - .5,
-        z = 10 / (.1 + Math.random());
-    for (var i = 0; i < n; i++) {
-      var w = (i / n - y) * z;
-      a[i] += x * Math.exp(-w * w);
-    }
-  }
+prerender.stop();
+svg.call(zoom);
 
-  var a = [], i;
-  for (i = 0; i < n; ++i) a[i] = 0;
-  for (i = 0; i < 5; ++i) bump(a);
-  return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
+function zoomed() {
+  svg.select(".x.axis").call(xAxis);
+  svg.select(".y.axis").call(yAxis);
 }
 
 },{"@fivethirtyeight/d3-pre":1,"d3":5}]},{},[163])
